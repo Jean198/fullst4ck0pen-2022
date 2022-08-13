a19d9ec218da66a9ae7f8e903065682186f59908
Jean198 <jeandedieu.niyigaba@gmail.com>
2022-08-13 12:12:47 +0300

    part1-exercise 1.11 unicafe step6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,31 +1,22 @@
 import { useState} from 'react'
-
 const Button=({updateGood,updateNeutral,updateBad})=>{
   return(
     <div>
-
       <button onClick={updateGood}>good</button>
       <button onClick={updateNeutral}>neutral</button>
       <button onClick={updateBad}>bad</button>
-
     </div>
   )
 }
-
 const StatisticLine=({text,value})=>{
-
   return(
     <>
     <td>{text}</td>
     <td>{value}</td>
     </>
   )
-
 }
-
-
 const Statistics =({good,neutral,bad})=>{
-
   if((good===0) && (neutral===0) && (bad===0) ){
     return(
       <div>
@@ -35,12 +26,11 @@ const Statistics =({good,neutral,bad})=>{
   }
   return(
     <div>
-
          <table>
-          <tr> <StatisticLine text="good" value={good}/></tr>
+          <tbody>
+          <tr><StatisticLine text="good" value={good}/></tr>
           <tr><StatisticLine text="neutral" value={neutral}/></tr>
           <tr><StatisticLine text="bad" value={bad}/></tr>
-
           <tr>
             <td>all</td>
             <td>{`${good+neutral+bad}`}</td>
@@ -51,67 +41,35 @@ const Statistics =({good,neutral,bad})=>{
           </tr>
           <tr>
             <td>positive</td>
-        
             <td>{`${good/(good+neutral+bad)*100}`} %</td>
           </tr>
+          </tbody>
          </table>
-        
-        
-        
-        <p> </p>
-        <p> </p>
-
     </div>
   )
 }
-
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  
-
   const updateGood=()=>{
        setGood(good+1)
   }
-
   const updateNeutral=()=>{
     setNeutral(neutral+1)
-   
 }
-
 const updateBad=()=>{
   setBad(bad+1)
- 
-  
 }
-
-
-
-
-
   return (
-    
     <div>
-
       <Button updateGood={updateGood} updateNeutral={updateNeutral} updateBad={updateBad}/>
-      
-
       <div>
         <h1>Statistics</h1>
-
         <Statistics good={good} neutral={neutral} bad={bad}/>
-
-        
-
-
       </div>
     </div>
-
   )
 }
-
 export default App
-
-
