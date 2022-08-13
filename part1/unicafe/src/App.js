9fe3c590205d19075c3b4c7155a4c1e54c172262
Jean198 <jeandedieu.niyigaba@gmail.com>
2022-08-13 11:31:07 +0300

    part1-exercise 1.8 unicafe step3

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,44 +1,43 @@
-import { useState, useEffect } from 'react'
+import { useState} from 'react'
+
+
+const Statistics =({good,neutral,bad})=>{
+  return(
+    <div>
+
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {`${good+neutral+bad}`}</p>
+        <p>average {`${(good*1+neutral*0+bad*(-1))/(good+neutral+bad)}`}</p>
+        <p>positive {`${good/(good+neutral+bad)*100}`} %</p>
+
+    </div>
+  )
+}
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [totalFeebacks,setTotalFeedbacks]=useState(0);
-  const [averageScore,setAverageScore]=useState(0);
   
 
   const updateGood=()=>{
        setGood(good+1)
-       setTotalFeedbacks(good+neutral+bad)
-       setAverageScore((good*1+neutral*0+bad*-1)/totalFeebacks)
   }
 
   const updateNeutral=()=>{
     setNeutral(neutral+1)
-    setTotalFeedbacks(good+neutral+bad)
-    setAverageScore((good*1+neutral*0+bad*-1)/totalFeebacks)
+   
 }
 
 const updateBad=()=>{
   setBad(bad+1)
-  setTotalFeedbacks(good+neutral+bad)
+ 
   
 }
 
-const updateTotal=()=>{
-  setTotalFeedbacks(good+neutral+bad)
-}
-
-const updateAverage=()=>{
-  setAverageScore((good*1+neutral*0+bad*-1)/totalFeebacks)
-}
-
-useEffect(() => {
-  updateTotal()
-  updateAverage()
-}, [good, neutral,bad,totalFeebacks])
 
 
 
@@ -53,12 +52,9 @@ useEffect(() => {
       <div>
         <h1>Statistics</h1>
 
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all {`${good+neutral+bad}`}</p>
-        <p>average {`${(good*1+neutral*0+bad*(-1))/(good+neutral+bad)}`}</p>
-        <p>positive {`${good/(good+neutral+bad)*100}`} %</p>
+        <Statistics good={good} neutral={neutral} bad={bad}/>
+
+        
 
 
       </div>
@@ -68,3 +64,5 @@ useEffect(() => {
 }
 
 export default App
+
+
