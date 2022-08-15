5dcd0a182a6e9c172e997df1833ba5f668b987ce
Jean198 <jeandedieu.niyigaba@gmail.com>
2022-08-15 15:21:22 +0300

    part2 exercise 2.9 The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,59 +1,73 @@
 import { useState } from 'react'
-
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone]=useState('')
+  const [filteredPersons,setFilteredPersons]=useState(persons)
+
 
   const addName=(event)=>{
        event.preventDefault();
-
        if(persons.some(person=>person.name===newName)){
         alert(`${newName} is already added to phonebook`)
        }else{
-
         const newPerson={
-          name:newName
+          name:newName,
+          number:newPhone
          }
-  
          setPersons(persons.concat(newPerson))
-  
-         
-
+         setFilteredPersons(filteredPersons.concat(newPerson))
        }
-
        setNewName('');
-       
+  }
 
-       
+
+  const filterPersons=(event)=>{
+    const searchWord=event.target.value
+    const filteredArray = persons.filter(person=>person.name.toLowerCase().includes((searchWord).toLowerCase()))
+    setFilteredPersons(filteredArray)
   }
 
   const handleNameChange=(event)=>{
     setNewName(event.target.value)
   }
 
+  const handlePhoneChange=(event)=>{
+    setNewPhone(event.target.value)
+  }
+
+
   return (
     <div>
       <h2>Phonebook</h2>
+      filter shown with <input type="text"  onChange={filterPersons}/>
       <form  onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          number: <input type="number" value={newPhone} onChange={handlePhoneChange}/>
         </div>
         <div>
           <button type="submit" >add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person)=>{
+      {filteredPersons.map((person)=>{
         return(
-          <h5 key={person.name}>{person.name}</h5>
+          <>
+          <p key={person.name}>{person.name} {person.number}</p>
+          </>
         )
       })}
-
       <div>debug: {newName}</div>
+      <div>debug: {newPhone}</div>
     </div>
   )
 }
-
 export default App
